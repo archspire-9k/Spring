37ba740d1b986b0b0a1c93896f7772cd20cda3f3
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-30 05:42:10 -0400

    Update

--- demo/src/main/java/com/example/demo/controller/ArrayController.java
+++ demo/src/main/java/com/example/demo/controller/ArrayController.java
@@ -1,14 +1,17 @@
-package com.example.demo;
+package com.example.demo.controller;
+
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 @Controller
 public class ArrayController {
     @GetMapping("/array")
-    public ArrayResponse index() {
-        return new ArrayResponse("Hi", List.of("A", "B", "C"));
+    public String index(Model model) {
+        model.addAttribute("array", new ArrayResponse("Hi", List.of("A", "B", "C")));
+        return "index";
     }
 
     record ArrayResponse(

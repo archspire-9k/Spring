ee94d1c00ec4117e9577eb26f6e07c2b77484e86
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-30 04:50:47 -0400

    update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -17,8 +20,9 @@ public class DemoApplication {
     }
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 
     @GetMapping("/")
@@ -29,7 +33,6 @@ public class DemoApplication {
     record ArrayResponse(
             String greet,
             List<String> names
-    ) {
-    }
+    ) {}
 
 }

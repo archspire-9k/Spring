8e93c64a89268c167d426e89364d44be0a5f52cf
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-03 10:49:31 -0400

    fix code

--- demo/src/main/java/com/example/demo/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -2,7 +2,14 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
 
 @Configuration
 @EnableWebSecurity
@@ -21,17 +28,20 @@ class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/customers*")
-                .hasRole("USER")
-                .anyRequest()
-                .permitAll();
-        http.oauth2Login()
-                .and()
-                .logout()
-                .addLogoutHandler(keycloakLogoutHandler)
-                .logoutSuccessUrl("/");
-        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.authorizeHttpRequests(auth ->
+                auth
+                        .requestMatchers("/customers*")
+                        .hasRole("USER")
+                        .anyRequest()
+                        .permitAll()
+        );
+        http.oauth2Login(Customizer.withDefaults())
+                .logout(logout ->
+                        logout.addLogoutHandler(keycloakLogoutHandler)
+                                .logoutSuccessUrl("/"));
+        http.oauth2ResourceServer((oauth2ResourceServer) ->
+        oauth2ResourceServer.jwt(Customizer.withDefaults())
+        );
         return http.build();
     }
 }

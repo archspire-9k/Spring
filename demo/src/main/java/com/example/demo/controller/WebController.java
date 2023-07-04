097c75e200f748e33217bfb8793e120907ceb97d
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-04 05:35:24 -0400

    move to subfolder

--- demo/src/main/java/com/example/demo/controller/WebController.java
+++ demo/src/main/java/com/example/demo/controller/WebController.java
@@ -1,5 +1,7 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import com.example.demo.Customer;
+import com.example.demo.CustomerDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;

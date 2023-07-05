3b8d5b5b72cccfa7d6c8544c6ee115029ca21891
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-05 04:38:45 -0400

    Add SecurityConfig

---
+++ springboot-keycloak-oauth2-client/src/main/java/com/example/springbootkeycloak/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.springbootkeycloak.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return null;
+    }
+}

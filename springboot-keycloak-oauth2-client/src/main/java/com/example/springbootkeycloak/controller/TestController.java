50efbb33c886c7540ba9ae46aa34c0e182ad7771
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-06 00:34:31 -0400

    No idea

--- springboot-keycloak-oauth2-client/src/main/java/com/example/springbootkeycloak/controller/TestController.java
+++ springboot-keycloak-oauth2-client/src/main/java/com/example/springbootkeycloak/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.example.springbootkeycloak.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +16,11 @@ public class TestController {
     }
     @GetMapping("/admin")
     public ResponseEntity<String> getAdmin(Principal principal) {
-        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
-        String userName = (String) token.getTokenAttributes().get("name");
-        String userEmail = (String) token.getTokenAttributes().get("email");
-        return ResponseEntity.ok("Hello Admin \nUser Name : " + userName + "\nUser Email : " + userEmail);
+
+        return ResponseEntity.ok("Hello Admin \nUser Name : ");
     }
     @GetMapping("/user")
     public ResponseEntity<String> getUser(Principal principal) {
-        JwtAuthenticationToken token = (JwtAuthenticationToken) principal;
-        String userName = (String) token.getTokenAttributes().get("name");
-        String userEmail = (String) token.getTokenAttributes().get("email");
-        return ResponseEntity.ok("Hello User \nUser Name : " + userName + "\nUser Email : " + userEmail);
+        return ResponseEntity.ok("Hello User \nUser Name : ");
     }
 }

e4ff7447e175444b357c80e59def6e1c878faa88
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-06 05:44:09 -0400

    remove unused imports

--- springboot-keycloak-oauth2-client/src/main/java/com/example/springbootkeycloak/controller/TestController.java
+++ springboot-keycloak-oauth2-client/src/main/java/com/example/springbootkeycloak/controller/TestController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
 @RestController
 @RequestMapping("/test")
 public class TestController {

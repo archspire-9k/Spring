20b5da8659b693d835e0446a422690883bde9c4e
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-05 23:04:59 -0400

    add permission for all user to enter index page

--- springboot-keycloak-oauth2-client/src/main/java/com/example/springbootkeycloak/security/SecurityConfig.java
+++ springboot-keycloak-oauth2-client/src/main/java/com/example/springbootkeycloak/security/SecurityConfig.java
@@ -1,20 +1,35 @@
 package com.example.springbootkeycloak.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public").permitAll()
-                        .anyRequest()
-                        .authenticated()
-                )
-                .oauth2Login(Customizer.withDefaults());
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .securityMatcher("/", "/error")
+                .authorizeHttpRequests( auth -> auth
+                                .anyRequest()
+                                .anonymous()
+                );
+//        http.exceptionHandling(e -> e
+//                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//        );
+//        http.oauth2Login(Customizer.withDefaults());
+//        http.sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
+
 }
